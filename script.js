cc66b0a18f13b7302cec5e0a9df8543be8524a7c
Prante524 <2221948@iub.edu.bd>
2025-03-22 13:24:32 +0600

    2nd commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function showAlert() {
+    alert("Welcome to our online store!");
+}
+
+function buyProduct(productName) {
+    alert("You have selected " + productName);
+}
+
+function submitForm(event) {
+    event.preventDefault();
+    alert("Your message has been sent!");
+}
